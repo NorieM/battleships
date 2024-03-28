acad631d0667d22d823e37c88c72f7e9be60f05b
Norie <norie.macewan@tiscali.co.uk>
2024-03-28 18:40:47 +0000

    Started creating board with unicode chars

--- battleships.py
+++ battleships.py
@@ -1,10 +1,16 @@
 
 def print_grid():
     print("   a b c d e f g h i j")
+    row = [u'\u2B1C']*10
+    board = []
     for i in range(10):
-        print (f'{i} {" *"*10}')
-        
+        board.append([u'\u2B1B']*10)
+    board[0][0] = u'\u2B1C'
+    board[0][1] = u'\u2B1C'
+    board[0][2] = u'\u2B1C'
+    board[0][3] = u'\u2B1C'
+    
+    for row in board:
+        print(''.join(row))
 
 print_grid()
-
-
