5d095c583c724fa9a895456fcea70bd759b256d5
Norie <norie.macewan@tiscali.co.uk>
2024-03-28 18:51:31 +0000

    Added docstrings

--- battleships.py
+++ battleships.py
@@ -1,16 +1,23 @@
+"""
+    Battleships in python
+"""
 
 def print_grid():
-    print("   a b c d e f g h i j")
-    row = [u'\u2B1C']*10
+    """
+    Creates board for Battleships
+    """
+    print("a b c d e f g h i j")
+    row = ['\u2B1C']*10
     board = []
-    for i in range(10):
-        board.append([u'\u2B1B']*10)
-    board[0][0] = u'\u2B1C'
-    board[0][1] = u'\u2B1C'
-    board[0][2] = u'\u2B1C'
-    board[0][3] = u'\u2B1C'
-    
+    for _ in range(10):
+        board.append(['\u2B1B']*10)
+    board[0][0] = '\u2B1C'
+    board[0][1] = '\u2B1C'
+    board[0][2] = '\u2B1C'
+    board[0][3] = '\u2B1C'
+
     for row in board:
         print(''.join(row))
+
 
 print_grid()
--- chessboard.py
+++ chessboard.py
@@ -1,20 +1,20 @@
 import numpy as np
 
 chrs = {
-    'b_checker': u'\u25FB',
-    'b_pawn': u'\u265F',
-    'b_rook': u'\u265C',
-    'b_knight': u'\u265E',
-    'b_bishop': u'\u265D',
-    'b_king': u'\u265A',
-    'b_queen': u'\u265B',
-    'w_checker': u'\u25FC',
-    'w_pawn': u'\u2659',
-    'w_rook': u'\u2656',
-    'w_knight': u'\u2658',
-    'w_bishop': u'\u2657',
-    'w_king': u'\u2654',
-    'w_queen': u'\u2655'
+    'b_checker': '\u25FB',
+    'b_pawn': '\u265F',
+    'b_rook': '\u265C',
+    'b_knight': '\u265E',
+    'b_bishop': '\u265D',
+    'b_king': '\u265A',
+    'b_queen': '\u265B',
+    'w_checker': '\u25FC',
+    'w_pawn': '\u2659',
+    'w_rook': '\u2656',
+    'w_knight': '\u2658',
+    'w_bishop': '\u2657',
+    'w_king': '\u2654',
+    'w_queen': '\u2655'
 }
 
 
